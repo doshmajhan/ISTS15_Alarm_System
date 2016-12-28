17d0e4c14e921fdcf6dd9a7fbeadb41a29a5d437
doshmajhan <cpc1007@rit.edu>
2016-12-28 15:50:07 -0400

    Added code to represent alarm and sensors, testing to see if code is right.

---
+++ alarm.py
@@ -0,0 +1,76 @@
+# program to represent the alarm system in a teams network
+# @author: Doshmajhan
+
+from pymodbus.server.async import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+from twisted.internet.task import LoopingCall
+from threading import Thread
+from time import sleep
+import os
+import argparse
+
+class Alarm(Thread):
+    def __init__(self, fileName=''):
+        Thread.__init__(self)
+        self.fileName = fileName
+        self.currentStat = -999
+        self.correctionFactor = 1;
+        self.enabled = True
+
+    def run(self):
+        while True:
+            stat = self.get_stat()
+            self.currentStat = stat
+            print "Current: " + str(self.currentStat) + " " + str(self.fileName)
+            sleep(5)
+
+    def get_stat(self):
+        return 1
+
+    def setEnabled(self, enabled):
+        self.enabled = enabled
+
+    def isEnabled(self):
+        return self.enabled
+
+def updating_writer(a):
+    context  = a[0]
+    register = 0
+    slave_id = 0x00
+    address  = 0x00
+    values = [int(pi.get_stat)]
+    context[slave_id].setValues(register,address,values)
+
+def main():
+    # initialize the four register types
+    store = ModbusSlaveContext(
+        di = ModbusSequentialDataBlock(0, [0]*100),
+        co = ModbusSequentialDataBlock(0, [0]*100),
+        hr = ModbusSequentialDataBlock(0, [0]*100),
+        ir = ModbusSequentialDataBlock(0, [0]*100))
+    context = ModbusServerContext(slaves=store, single=True)
+
+    identity = ModbusDeviceIdentification()
+    identity.VendorName  = 'SPARSA'
+    identity.ProductCode = 'SP'
+    identity.VendorUrl   = 'http://doshmajhan.com/magicschoolbus'
+    identity.ProductName = 'SPARSA Alarm Sensor'
+    identity.ModelName   = 'SP_1337'
+    identity.MajorMinorRevision = '1.0'
+    pi.start()
+    time = 5 # 5 seconds delaytime = 5 # 5 seconds delay
+    loop = LoopingCall(f=updating_writer, a=(context,))
+    loop.start(time, now=False) # initially delay by time
+    StartTcpServer(context, identity=identity, address=(args.address, 502))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Listens on a specified interface for modbus connections')
+    parser.add_argument('-a', dest='address', help='The IP address to bind to', required=True)
+
+    # read arguments (the name of the file to copy)
+    args = parser.parse_args()
+    pi = Alarm()
+    main()
---
+++ alarm_server.py
@@ -0,0 +1,84 @@
+# Server to handle status updates from the alarm clients
+# @author: Doshmajhan
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+import logging
+import argparse
+import time
+import sys
+import random
+import time
+
+class sensor():
+    def __init__(self, team, addr, stat):
+        self.team = team
+        #self.filename = str(team) + "-temp.txt"
+        self.addr = addr
+        self.stat = self.get_stat(self.addr)
+        self.sleeping = False
+        self.timeout = 0
+
+    def get_stat(self, addr):
+        # connect to modbus slave
+        try:
+            client = ModbusClient(addr, port=502)
+            client.connect()
+            rr = client.read_holding_registers(0x00,1,unit=1)
+            stat = rr.registers[0]
+            return stat
+        except:
+            # if unable to connect, return None
+            log_stuff("Unable to connect to " + self.addr)
+            return None
+
+    # set alarm either off or on
+    def set_alarm(self):
+        try:
+            client = ModbusClient(addr, port=502)
+            client.connect()
+            if self.stat != 1:
+                rq = client.write_registers(0, 0)
+                print rq.function_code
+            else:
+                rq = client.write_registers(0., 1)
+                print rq.function_code
+
+        except:
+            # if unable to connect, return None
+            log_stuff("Unable to connect to " + self.addr)
+            return None
+
+        return "Success"
+
+def log_stuff(message):
+    with open('scada.log','a+') as f:
+        now = time.strftime("%c")
+        f.write(now + " - " + message + "\n")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Poll the alarm sensor of a given team.')
+    parser.add_argument('-t', help='The URL to start scraping from', dest="team",  required="True")
+    parser.add_argument('-a', help='Address of the alarm sensor to poll', dest="address", required="True")
+    args = parser.parse_args()
+
+    while True:
+        sensor_obj = sensor(args.team, args.address, 0)
+        f = open('teams_status.txt', 'w')
+
+        if(sensor_obj.stat != None and sensor_obj.stat == 1):
+            log_stuff("Setting team number " + str(sensor_obj.team) + "to ON in " + str(sensor_obj.filename))
+            f.write(str(sensor_obj.stat))
+
+        elif (sensor_obj.stat != None and sensor_obj.stat != 0):
+            log_stuff("Setting team number " + str(sensor_obj.team) + "to OFF in " + str(sensor_obj.filename))
+            f.write(str(sensor_obj.stat))
+
+        else:
+            f.write("NULL")
+
+        f.close()
+        sensor_obj.set_alarm()
+
+        time.sleep(10)
+
+    print "Exiting"
